bf4f7ba288b613a12aa79425c145993a6edd76b6
secwi032-sudo <secwi032@gmail.com>
2026-02-03 17:34:01 +0900

    로그인+공고

--- src/main/java/com/example/agent_rnd/repository/NoticeHashtagRepository.java
+++ src/main/java/com/example/agent_rnd/repository/NoticeHashtagRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NoticeHashtagRepository extends JpaRepository<NoticeHashtag, Long> {
-    List<NoticeHashtag> findByNotice_NoticeId(Long noticeId);
+    List<NoticeHashtag> findByProjectNotice_NoticeId(Long noticeId);
 }
